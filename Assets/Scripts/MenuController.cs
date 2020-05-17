72119324b7aee47644bcc448ef6f7ed65d7fe577
jwcorle <jcorley.w@gmail.com>
2020-05-17 19:26:26 -0400

    Added main menu.

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+    public GameObject highScores;
+
+    public void HighScores() {
+        highScores.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void Back() {
+        highScores.SetActive(false);
+        gameObject.SetActive(true);
+    }
+
+    public void PlayGame() {
+        SceneManager.LoadScene("Main");
+    }
+}
